8a78067e3ff21d90a33f1d57626ab9fe58532a5f
taran-duba <72531052+taran-duba@users.noreply.github.com>
2021-02-17 15:14:22 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,58 @@
+Webcam.set({
+    width:350,
+    height:300,
+    image_format: 'png',
+    png_quality: 90
+});
+camera = document.getElementById("camera");
+Webcam.attach('#camera');
+
+function take_snapshot() {
+    Webcam.snap(function(data_uri) {
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="' + data_uri + '"/>';
+    });
+}
+console.log('ml5 version', ml5.version);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/Hl1u-KLCA/model.json', modelLoaded);
+function modelLoaded() {
+    console.log('model loaded');
+}
+function speak() {
+    var synth = window.speechSynthesis;
+    speak_data1 = "The prediction is " + prediction_1;
+    var utterThis = new SpeechSynthesisUtterance(speak_data1);
+    synth.speak(utterThis);
+}
+function check() {
+    img = document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
+}
+function gotResult(error, results) {
+    if (error) {
+        console.error(error);
+    } else {
+        console.log(results);
+        document.getElementById("result_emotion_name").innerHTML = results[0].label;
+        prediction_1 = results[0].label;
+        prediction_2 = results[1].label;
+        speak();
+        if (results[0].label == "Thumbs Up") {
+            document.getElementById("update_emoji").innerHTML = "&#128077;";
+        }
+        if (results[0].label == "Thumbs Down") {
+            document.getElementById("update_emoji").innerHTML = "&#128078;";
+        }
+        if (results[0].label == "Peace") {
+            document.getElementById("update_emoji").innerHTML = "&#9996;";
+        }
+        if (results[0].label == "Super") {
+            document.getElementById("update_emoji").innerHTML = "&#128076;";
+        }
+        if (results[0].label == "Fist") {
+            document.getElementById("update_emoji").innerHTML = "&#9994;";
+        }
+        if (results[0].label == "Clap") {
+            document.getElementById("update_emoji").innerHTML = "&#128079;";
+        }
+    }
+}
